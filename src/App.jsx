d0c1cd44b8c5366069b83de7c9def12819a7e8c0
Khoirul Anam <152753834+Namskt@users.noreply.github.com>
2024-11-11 00:01:06 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,24 @@ function App() {
             alt="..."
           />
         </div>
-        <div className="flex flex-col justify-center items-center relative">
-        <div className="relative">
+        <div className="flex flex-col justify-center items-center relative>
           <a
             href="https://fbs.partners?ibl=865482&ibp=33988576"
             className="py-3 px-5 bg-green-700 rounded text-white text-sm font-semibold hover:bg-green-800 hover:text-white"
           >
             BROKER YANG SERING GUA PAKEK
           </a>
-        </div>
-        <div className="relative">
-         <a
+           <a
             href="https://one.exnesstrack.org/boarding/sign-up/a/7di01z8hol?lng=id"
             className="py-3 px-5 bg-green-700 rounded text-white text-sm font-semibold hover:bg-green-800 hover:text-white"
           >
             BROKER DENGAN AKUN ZERO SPREAD RENDAH
           </a>
-        </div>
-        </div>
-      </div>
+          </div>
+        
+     
+        
+      
     </div>
   );
 }
