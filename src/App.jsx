e3ff11aeafe3897dee55303b4f5c565bd0c4ca5c
Khoirul Anam <152753834+Namskt@users.noreply.github.com>
2024-11-10 23:57:00 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             alt="..."
           />
         </div>
-        <div className="max-w-lg">
+        <div className="max-w-lg flex justify-center items-center">
         <div>
           <a
             href="https://fbs.partners?ibl=865482&ibp=33988576"
